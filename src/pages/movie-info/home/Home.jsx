be2c473497241c5a31425154719e1cac3716a610
Rami jawadi <ramijawadi104@gmail.com>
2022-12-07 16:22:30 +0100

    Add debounce function to control the search & add progress loader && alert component ...

--- src/pages/movie-info/home/Home.jsx
+++ src/pages/movie-info/home/Home.jsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import { SearchContextProvider } from '../../../contexts/SearchContext'
 
 import FoundedMovies from './components/FoundedMovies'
 import SearchBar from './components/SearchBar'
@@ -7,8 +8,10 @@ import SearchBar from './components/SearchBar'
 const Home = () => {
   return (
     <Box>
+      <SearchContextProvider>
     <SearchBar />
     <FoundedMovies/>
+    </SearchContextProvider>
     </Box>
   )
 }
--- src/pages/movie-info/home/components/SearchBar.jsx
+++ src/pages/movie-info/home/components/SearchBar.jsx
@@ -1,13 +1,21 @@
 import { Box, TextField } from '@mui/material'
-import React from 'react'
+import React , {useContext} from 'react'
 import SearchIcon from '@mui/icons-material/Search';
-
+import { SearchContext } from '../../../../contexts/SearchContext';
+import lodash from 'lodash'
 
 const SearchBar = () => {
+  const {setSearchWord}= useContext(SearchContext);
+const handleChange =(e) => {
+
+  setSearchWord(e.target.value);
+}
+
+const debounce = lodash.debounce(handleChange , 500);
   return (
     <Box sx={{ display : "flex" , alignItems:"flex-end"}}>
     <SearchIcon fontSize='large' sx={{marginRight:"2px"}} color = 'primary' />
-    <TextField id="standard-basic" label="Search" variant="standard" fullWidth color='primary'/>
+    <TextField id="standard-basic" label="Search" variant="standard" fullWidth color='primary' onChange={debounce}/>
     </Box>
   )
 }
